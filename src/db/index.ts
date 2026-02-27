dbb0c468cad11c4c18128d24b77e2ec8680de1e1
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-27 19:09:28 +0530

    config (drizzle): add drizzle configs

---
+++ src/db/index.ts
@@ -0,0 +1,18 @@
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
+// import * as schema from './schema'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set')
+}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes('supabase')
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
+})
+
+// export const db = drizzle(pool, { schema })
