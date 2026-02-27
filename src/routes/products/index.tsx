261e645ae10a47bdde62d7469a60ae942605046a
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-27 18:31:33 +0530

    config: add Middleware

--- src/routes/products/index.tsx
+++ src/routes/products/index.tsx
@@ -6,9 +6,45 @@ import {
     CardTitle,
 } from '@/components/ui/card'
 import { createFileRoute } from '@tanstack/react-router'
+import { createMiddleware, createServerFn, json } from '@tanstack/react-start'
+
+const fetchProducts = createServerFn({ method: 'GET' }).handler(async () => {
+    // Map products to ensure all fields match ProductCard expectations
+    const response = await fetch('https://fakestoreapi.com/products')
+
+    return response.json()
+})
+
+const loggerMiddleware = createMiddleware().server(
+    async ({ next, request }) => {
+        console.log(
+            '---loggerMiddleware---',
+            request.url,
+            'from',
+            request.headers.get('origin'),
+        )
+        return next()
+    },
+)
 
 export const Route = createFileRoute('/products/')({
     component: RouteComponent,
+    loader: async (ctx) => {
+        console.log('---loader--')
+        return fetchProducts()
+    },
+    server: {
+        middleware: [loggerMiddleware],
+        handlers: {
+            POST: async ({ request }) => {
+                const body = await request.json()
+                return json({
+                    message: 'Hello, world from POST request!',
+                    body,
+                })
+            },
+        },
+    },
 })
 
 function RouteComponent() {
