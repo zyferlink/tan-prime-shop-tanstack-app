dc76dc7526d5efec14f80fafb573219173845090
NovaLogics <rajitha.shavinda@gmail.com>
2026-02-28 00:23:52 +0530

    config (drizzle): update drizzle configs

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { Pool } from 'pg'
-// import * as schema from './schema'
+import * as schema from './schema'
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set')
@@ -15,4 +15,4 @@ const pool = new Pool({
     : false,
 })
 
-// export const db = drizzle(pool, { schema })
+export const db = drizzle(pool, { schema })
