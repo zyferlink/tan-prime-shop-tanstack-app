bcb8d13b7e367cca7ffdba3ad66d4aaa8a68142e
NovaLogics <rajitha.shavinda@gmail.com>
2026-03-01 14:08:35 +0530

    chore: apply consistent code style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,70 +1,66 @@
-import { useQuery } from '@tanstack/react-query'
-import { Link } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
-import { ShoppingBag } from 'lucide-react'
+import { useQuery } from '@tanstack/react-query';
+import { Link } from '@tanstack/react-router';
+import { createServerFn } from '@tanstack/react-start';
+import { ShoppingBag } from 'lucide-react';
 
-const getCartItemsCount = createServerFn({ method: 'GET' }).handler(
-    async () => {
-        const { getCartItemsCount } = await import('@/data/cart.server')
-        const data = await getCartItemsCount()
-        return data
-    },
-)
+const getCartItemsCount = createServerFn({ method: 'GET' }).handler(async () => {
+  const { getCartItemsCount } = await import('@/data/cart.server');
+  const data = await getCartItemsCount();
+  return data;
+});
 
 export default function Header() {
-    const { data: cartItemsData } = useQuery({
-        queryKey: ['cart-items-data'],
-        queryFn: () => getCartItemsCount(),
-    })
-    return (
-        <header className="sticky top-0 z-40 border-b border-slate-200 bg-white/80 backdrop-blur dark:border-slate-800 dark:bg-slate-950/80">
-            <div className="mx-auto max-w-6xl px-4 py-3 items-center justify-between flex">
-                <div className="flex items-center gap-3">
-                    <Link to="/" className="flex items-center gap-2">
-                        <div className="flex h-10 w-10 items-center justify-center rounded-xl bg-slate-900 text-white dark:bg-white dark:text-slate-800">
-                            <ShoppingBag size={20} />
-                        </div>
-
-                        <div className="flex flex-col">
-                            <span className="text-sm font-semibold text-slate-900 dark:text-white">
-                                TanPrime Shop
-                            </span>
-                        </div>
-                    </Link>
-
-                    <nav className="hidden items-center gap-3 text-sm font-medium text-slate-700 dark:text-slate-200 sm:flex">
-                        <Link
-                            to="/"
-                            className="rounded-lg px-3 py-1 transition hover:bg-slate-100 dark:hover:bg-slate-800"
-                        >
-                            Home
-                        </Link>
-                        <Link
-                            to="/products"
-                            className="rounded-lg px-3 py-1 transition hover:bg-slate-100 dark:hover:bg-slate-800"
-                        >
-                            Products
-                        </Link>
-                        <Link to="/products/create-product">
-                            Create Product
-                        </Link>
-                    </nav>
-                </div>
-                <div className="flex items-center gap-2">
-                    <Link
-                        to="/cart"
-                        className="inline-flex items-center gap-2 rounded-full border border-slate-200 bg-white px-4 py-2 text-xs font-semibold text-slate-800 shadow-sm transition hover:-translate-y-0.5 hover:shadow-md"
-                    >
-                        <span>Cart</span>
-                        <span className="flex h-6 min-w-6 items-center justify-center rounded-full bg-slate-900 px-2 text-[11px] font-bold text-white">
-                            {cartItemsData?.count ?? 0}
-                        </span>
-                        <span className="hidden text-[11px] font-medium tex-slate-500 sm:inline">
-                            ${cartItemsData?.total.toFixed(2) ?? 0}
-                        </span>
-                    </Link>
-                </div>
+  const { data: cartItemsData } = useQuery({
+    queryKey: ['cart-items-data'],
+    queryFn: () => getCartItemsCount(),
+  });
+  return (
+    <header className="sticky top-0 z-40 border-b border-slate-200 bg-white/80 backdrop-blur dark:border-slate-800 dark:bg-slate-950/80">
+      <div className="mx-auto max-w-6xl px-4 py-3 items-center justify-between flex">
+        <div className="flex items-center gap-3">
+          <Link to="/" className="flex items-center gap-2">
+            <div className="flex h-10 w-10 items-center justify-center rounded-xl bg-slate-900 text-white dark:bg-white dark:text-slate-800">
+              <ShoppingBag size={20} />
             </div>
-        </header>
-    )
+
+            <div className="flex flex-col">
+              <span className="text-sm font-semibold text-slate-900 dark:text-white">
+                TanPrime Shop
+              </span>
+            </div>
+          </Link>
+
+          <nav className="hidden items-center gap-3 text-sm font-medium text-slate-700 dark:text-slate-200 sm:flex">
+            <Link
+              to="/"
+              className="rounded-lg px-3 py-1 transition hover:bg-slate-100 dark:hover:bg-slate-800"
+            >
+              Home
+            </Link>
+            <Link
+              to="/products"
+              className="rounded-lg px-3 py-1 transition hover:bg-slate-100 dark:hover:bg-slate-800"
+            >
+              Products
+            </Link>
+            <Link to="/products/create-product">Create Product</Link>
+          </nav>
+        </div>
+        <div className="flex items-center gap-2">
+          <Link
+            to="/cart"
+            className="inline-flex items-center gap-2 rounded-full border border-slate-200 bg-white px-4 py-2 text-xs font-semibold text-slate-800 shadow-sm transition hover:-translate-y-0.5 hover:shadow-md"
+          >
+            <span>Cart</span>
+            <span className="flex h-6 min-w-6 items-center justify-center rounded-full bg-slate-900 px-2 text-[11px] font-bold text-white">
+              {cartItemsData?.count ?? 0}
+            </span>
+            <span className="hidden text-[11px] font-medium tex-slate-500 sm:inline">
+              ${cartItemsData?.total.toFixed(2) ?? 0}
+            </span>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/RecommendedProducts.tsx
+++ src/components/RecommendedProducts.tsx
@@ -1,21 +1,21 @@
-import { ProductSelect } from '@/db/schema'
-import { ProductCard } from './ProductCard'
-import { use } from 'react'
+import { ProductSelect } from '@/db/schema';
+import { ProductCard } from './ProductCard';
+import { use } from 'react';
 
 export function RecommendedProducts({
-    recommendedProducts,
+  recommendedProducts,
 }: {
-    recommendedProducts: Promise<ProductSelect[]>
+  recommendedProducts: Promise<ProductSelect[]>;
 }) {
-    const recommendedProductsData = use(recommendedProducts)
-    return (
-        <div>
-            <h2 className="text-2xl font-bold my-4">Recommended Products</h2>
-            <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-                {recommendedProductsData.map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
-        </div>
-    )
+  const recommendedProductsData = use(recommendedProducts);
+  return (
+    <div>
+      <h2 className="text-2xl font-bold my-4">Recommended Products</h2>
+      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        {recommendedProductsData.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
