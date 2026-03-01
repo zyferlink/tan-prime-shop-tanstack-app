65488d089fd150691a08684db34646d7c2a3c481
NovaLogics <rajitha.shavinda@gmail.com>
2026-03-01 13:54:16 +0530

    chore: apply consistent code style

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,78 +1,75 @@
-import {
-    HeadContent,
-    Scripts,
-    createRootRoute,
-    createRootRouteWithContext,
-} from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-import Header from '../components/Header'
+import { HeadContent, Scripts, createRootRouteWithContext } from '@tanstack/react-router';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
+import { TanStackDevtools } from '@tanstack/react-devtools';
+import { type QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { useEffect } from 'react'
 
-import appCss from '../styles.css?url'
-import { type QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Header from '../components/Header';
+// import { client } from '../lib/appwrite'
 
-const THEME_INIT_SCRIPT = `(function(){try{var stored=window.localStorage.getItem('theme');var mode=(stored==='light'||stored==='dark'||stored==='auto')?stored:'auto';var prefersDark=window.matchMedia('(prefers-color-scheme: dark)').matches;var resolved=mode==='auto'?(prefersDark?'dark':'light'):mode;var root=document.documentElement;root.classList.remove('light','dark');root.classList.add(resolved);if(mode==='auto'){root.removeAttribute('data-theme')}else{root.setAttribute('data-theme',mode)}root.style.colorScheme=resolved;}catch(e){}})();`
+import appCss from '../styles.css?url';
 
-export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
-    {
-        head: () => ({
-            meta: [
-                {
-                    charSet: 'utf-8',
-                },
-                {
-                    name: 'viewport',
-                    content: 'width=device-width, initial-scale=1',
-                },
-                {
-                    title: 'TanStack Start Starter',
-                },
-            ],
-            links: [
-                {
-                    rel: 'stylesheet',
-                    href: appCss,
-                },
-            ],
-        }),
-        shellComponent: RootDocument,
-    },
-)
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanPrime Shop',
+      },
+      {
+        description: 'TanPrime Shop is a platform for buying and selling products',
+      },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+
+  shellComponent: RootDocument,
+});
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-    const { queryClient } = Route.useRouteContext()
+  const { queryClient } = Route.useRouteContext();
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            <html lang="en" suppressHydrationWarning>
-                <head>
-                    <script
-                        dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }}
-                    />
-                    <HeadContent />
-                </head>
-                <body className="font-sans antialiased wrap-anywhere ">
-                    <div className="min-h-screen bg-slate-100 text-slate-900 dark:bg-slate-950 dark:text-white">
-                        <Header />
-                        <main className="mx-auto max-w-6xl px-4 py-6">
-                            {children}
-                        </main>
-                    </div>
+  // Ping backend to verify setup when app opens
+  //   useEffect(() => {
+  //     client.ping()
+  //   }, [])
 
-                    <TanStackDevtools
-                        config={{
-                            position: 'bottom-right',
-                        }}
-                        plugins={[
-                            {
-                                name: 'Tanstack Router',
-                                render: <TanStackRouterDevtoolsPanel />,
-                            },
-                        ]}
-                    />
-                    <Scripts />
-                </body>
-            </html>
-        </QueryClientProvider>
-    )
+  return (
+    <QueryClientProvider client={queryClient}>
+      <html lang="en">
+        <head>
+          <HeadContent />
+        </head>
+        <body className="font-sans antialiased wrap-anywhere">
+          <div className="min-h-screen bg-slate-100 text-slate-900 dark:bg-slate-950 dark:text-white">
+            <Header />
+            <main className="mx-auto max-w-6xl px-4 py-6">{children}</main>
+          </div>
+          <TanStackDevtools
+            config={{
+              position: 'bottom-right',
+            }}
+            plugins={[
+              {
+                name: 'Tanstack Router',
+                render: <TanStackRouterDevtoolsPanel />,
+              },
+            ]}
+          />
+          <Scripts />
+        </body>
+      </html>
+    </QueryClientProvider>
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,94 +1,81 @@
-import { ProductCard } from '@/components/ProductCard'
-import {
-    Card,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card'
-import { ProductSelect } from '@/db/schema'
-import { createFileRoute, Link } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/react-start'
-import { ArrowRightIcon } from 'lucide-react'
+import { ProductCard } from '@/components/ProductCard';
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { createServerFn } from '@tanstack/react-start';
+import { ArrowRightIcon } from 'lucide-react';
 
 const fetchProductsFn = createServerFn({ method: 'GET' }).handler(async () => {
-    const { getRecommendedProducts } = await import('@/data/products')
-    const products = await getRecommendedProducts()
-    return products
-})
+  const { getRecommendedProducts } = await import('@/data/products');
+  const products = await getRecommendedProducts();
+  return products;
+});
 
 export const Route = createFileRoute('/')({
-    component: App,
-    loader: async () => {
-        // This runs on server during SSR AND on client during navigation
-        return fetchProductsFn()
-    },
-})
+  component: App,
+  loader: async () => {
+    // This runs on server during SSR AND on client during navigation
+    return fetchProductsFn();
+  },
+});
 
 async function App() {
-    const products = Route.useLoaderData()
+  const products = Route.useLoaderData();
 
-    console.log('--client products--', products)
-    return (
-        <div className="space-y-12 bg-linear-to-b from-slate-50 via-white to-slate-50 p-6">
-            <section>
-                <Card className="p-8 shadow-md bg-white/80">
-                    <p className="text-sm font-semibold uppercase tracking-wide text-blue-600">
-                        Your favourite e-commerce store
-                    </p>
-                    <CardTitle className="text-4xl font-bold leading-tight text-slate-900 dark:text-white max-w-2xl">
-                        <h1>
-                            TanPrime Shop - Your one stop shop for all your
-                            needs
-                        </h1>
-                    </CardTitle>
-                    <CardDescription>
-                        <Link
-                            to="/products"
-                            className="inline-flex items-center gap-2 rounded-full bg-slate-900 px-5 py-3 text-sm font-semibold text-white shadow-lg transition hover:-translate-y-0.5 hover:shadow-xl"
-                        >
-                            Browse products
-                            <ArrowRightIcon size={16} />
-                        </Link>
-                    </CardDescription>
-                </Card>
-            </section>
+  console.log('--client products--', products);
+  return (
+    <div className="space-y-12 bg-linear-to-b from-slate-50 via-white to-slate-50 p-6">
+      <section>
+        <Card className="p-8 shadow-md bg-white/80">
+          <p className="text-sm font-semibold uppercase tracking-wide text-blue-600">
+            Your favourite e-commerce store
+          </p>
+          <CardTitle className="text-4xl font-bold leading-tight text-slate-900 dark:text-white max-w-2xl">
+            <h1>StartShop - Your one-stop shop for all your needs</h1>
+          </CardTitle>
+          <CardDescription>
+            <Link
+              to="/products"
+              className="inline-flex items-center gap-2 rounded-full bg-slate-900 px-5 py-3 text-sm font-semibold text-white shadow-lg transition hover:-translate-y-0.5 hover:shadow-xl"
+            >
+              Browse products
+              <ArrowRightIcon size={16} />
+            </Link>
+          </CardDescription>
+        </Card>
+      </section>
 
-            <section className="space-y-4 max-w-6xl mx-auto">
-                <Card className="p-6 shadow-md bg-white/80">
-                    <div className="flex items-center justify-between">
-                        <div>
-                            <CardHeader className="px-0">
-                                <p className="text-xs font-semibold uppercase tracking-wide text-blue-600">
-                                    Recommended
-                                </p>
-                                <CardTitle className="text-2xl font-semibold text-slate-900">
-                                    Starter picks from the catalog
-                                </CardTitle>
-                            </CardHeader>
-                            <CardDescription className="text-sm text-slate-600">
-                                Curated items to try the cart and detail pages
-                                quickly.
-                            </CardDescription>
-                        </div>
-                        <div>
-                            <Link
-                                to="/products"
-                                className="hidden items-center gap-2 rounded-full border border-slate-200 px-4 py-2 text-xs font-semibold text-slate-700 sm:inline-flex transition hover:-translate-y-0.5 hover:shadow-xl"
-                            >
-                                View All <ArrowRightIcon size={14} />
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 mb-6">
-                        {products.map((product: ProductSelect, index) => (
-                            <ProductCard
-                                product={product}
-                                key={`product-${index}`}
-                            />
-                        ))}
-                    </div>
-                </Card>
-            </section>
-        </div>
-    )
+      <section className="space-y-4 max-w-6xl mx-auto">
+        <Card className="p-6 shadow-md bg-white/80">
+          <div className="flex items-center justify-between">
+            <div>
+              <CardHeader className="px-0">
+                <p className="text-xs font-semibold uppercase tracking-wide text-blue-600">
+                  Recommended
+                </p>
+                <CardTitle className="text-2xl font-semibold text-slate-900">
+                  Starter picks from the catalog
+                </CardTitle>
+              </CardHeader>
+              <CardDescription className="text-sm text-slate-600">
+                Curated items to try the cart and detail pages quickly.
+              </CardDescription>
+            </div>
+            <div>
+              <Link
+                to="/products"
+                className="hidden items-center gap-2 rounded-full border border-slate-200 px-4 py-2 text-xs font-semibold text-slate-700 sm:inline-flex transition hover:-translate-y-0.5 hover:shadow-xl"
+              >
+                View All <ArrowRightIcon size={14} />
+              </Link>
+            </div>
+          </div>
+          <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 mb-6">
+            {products.map((product, index) => (
+              <ProductCard product={product} key={`product-${index}`} />
+            ))}
+          </div>
+        </Card>
+      </section>
+    </div>
+  );
 }
